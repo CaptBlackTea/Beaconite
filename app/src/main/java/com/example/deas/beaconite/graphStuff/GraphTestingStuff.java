0b2500faf52eda825a6605560d9008c108feccda
Dea Svoboda <dea-heike.svoboda@stud.uni-bamberg.de>
2016-11-25 22:08:08 +0100

    Graph has now custom labled edges and in the DOT output the graph edges have lables and colors
    * added a custom edges class
    ** labels for edges are enums: NONE, MUSTNOT, REQUIRED
    ** usage will be to assign semantic to graphs and which edges are required (physically or in the game) etc

--- app/src/main/java/com/example/deas/beaconite/graphStuff/GraphTestingStuff.java
+++ app/src/main/java/com/example/deas/beaconite/graphStuff/GraphTestingStuff.java
@@ -1,12 +1,15 @@
 package com.example.deas.beaconite.graphStuff;
 
 import org.jgrapht.UndirectedGraph;
-import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.ClassBasedEdgeFactory;
 import org.jgrapht.graph.SimpleGraph;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import static com.example.deas.beaconite.graphStuff.EdgeLabel.MUSTNOT;
+import static com.example.deas.beaconite.graphStuff.EdgeLabel.REQUIRED;
 
 /**
  * Data structure representing an undirected graph. Such a graph has vertices (v) and edges (e).
@@ -26,7 +29,12 @@ public class GraphTestingStuff {
 		System.out.println("Ahoi");
 
 
-		UndirectedGraph<Vertex, DefaultEdge> testGraph = new SimpleGraph<>(DefaultEdge.class);
+//		UndirectedGraph<Vertex, DefaultEdge> testGraph = new SimpleGraph<>(DefaultEdge.class);
+
+		UndirectedGraph<Vertex, LabeledEdge> testGraph = new SimpleGraph<>(new
+				ClassBasedEdgeFactory<Vertex, LabeledEdge>(LabeledEdge.class));
+
+
 
 		// make some nodes/vertices
 		Vertex v1 = new Vertex("v1");
@@ -34,7 +42,7 @@ public class GraphTestingStuff {
 		Vertex v3 = new Vertex("v3");
 		Vertex v4 = new Vertex("v4");
 
-		// put vetices in the graph
+		// put vertices in the graph
 		testGraph.addVertex(v1);
 		testGraph.addVertex(v2);
 		testGraph.addVertex(v3);
@@ -42,10 +50,10 @@ public class GraphTestingStuff {
 
 
 		// make edges between vertices
-		testGraph.addEdge(v1, v2);
-		testGraph.addEdge(v1, v4);
-		testGraph.addEdge(v2, v4);
-		testGraph.addEdge(v2, v3);
+		testGraph.addEdge(v1, v2, new LabeledEdge(v1, v2));
+		testGraph.addEdge(v1, v4, new LabeledEdge(v1, v4, MUSTNOT));
+		testGraph.addEdge(v2, v4, new LabeledEdge(v2, v4, REQUIRED));
+		testGraph.addEdge(v2, v3, new LabeledEdge(v2, v3));
 
 		System.out.println(testGraph);
 		plotTestGraph(testGraph);
@@ -53,14 +61,19 @@ public class GraphTestingStuff {
 	}
 
 
-	public static void plotTestGraph(UndirectedGraph<Vertex, DefaultEdge> graph) {
+	public static void plotTestGraph(UndirectedGraph<Vertex, LabeledEdge> graph) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("graph {");
 
-		for (DefaultEdge de : graph.edgeSet()) {
+		for (LabeledEdge de : graph.edgeSet()) {
 			sb.append(graph.getEdgeSource(de).getName());
 			sb.append(" -- ");
 			sb.append(graph.getEdgeTarget(de).getName());
+
+			// for labeling the edges
+			sb.append(" [label=" + de.getLabel());
+			sb.append(" color=" + chooseEdgeColor(de.getLabel()));
+			sb.append("];");
 
 			sb.append("\n");
 		}
@@ -75,6 +88,23 @@ public class GraphTestingStuff {
 		} catch (IOException e) {
 
 		}
+	}
+
+	private static String chooseEdgeColor(Enum label) {
+
+		String colorString;
+
+		if (label.equals(EdgeLabel.REQUIRED)) {
+			colorString = "green";
+
+		} else if (label.equals(EdgeLabel.MUSTNOT)) {
+			colorString = "red";
+
+		} else {
+			colorString = "black";
+		}
+
+		return colorString;
 	}
 
 }
