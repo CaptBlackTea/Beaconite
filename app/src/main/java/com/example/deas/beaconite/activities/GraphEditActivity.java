454fd7d7a6b51f63a9a0c980d53c82c902bf40d8
Dea Svoboda <dea-heike.svoboda@stud.uni-bamberg.de>
2016-12-02 17:53:46 +0100

    GraphEditActivity binds to serivce

--- app/src/main/java/com/example/deas/beaconite/activities/GraphEditActivity.java
+++ app/src/main/java/com/example/deas/beaconite/activities/GraphEditActivity.java
@@ -1,15 +1,105 @@
 package com.example.deas.beaconite.activities;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.example.deas.beaconite.BeaconDataService;
 import com.example.deas.beaconite.R;
 
 public class GraphEditActivity extends AppCompatActivity {
+
+	protected static final String TAG = "GraphEditActivity";
+
+	// the Service
+	private BeaconDataService mService;
+	private boolean mIsBound = false;
+	private Intent beaconDataServiceIntent;
+
+	/**
+	 * Defines callbacks for service binding, passed to bindService()
+	 */
+	private ServiceConnection mConnection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// We've bound to BeaconDataService, cast the IBinder and get BeaconDataService instance
+
+			// This is called when the connection with the service has been
+			// established, giving us the service object we can use to
+			// interact with the service.  Because we have bound to a explicit
+			// service that we know is running in our own process, we can
+			// cast its IBinder to a concrete class and directly access it.
+
+			BeaconDataService.BeaconDataBinder serviceBinder = (BeaconDataService.BeaconDataBinder)
+					service;
+			mService = serviceBinder.getService();
+			mIsBound = true;
+
+			Log.d(TAG, "------ on Service connected was called. mService, mIsBound: " + mService
+					+ ", " + mIsBound);
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			mIsBound = false;
+			Log.d(TAG, "********** SERVICE WAS DISCONNECTED!");
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.graph_edit_activty);
+
+
+		beaconDataServiceIntent = new Intent(this, BeaconDataService.class);
+
+		Log.d(TAG, "#### CREATE");
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		// Bind to BeaconDataService
+		bindService(beaconDataServiceIntent, mConnection, Context.BIND_AUTO_CREATE);
+		Log.d(TAG, "#### START: mIsBound: " + mIsBound + " mService: " + mService);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		if (mIsBound) {
+			unbindService(mConnection);
+			mIsBound = false;
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+
+		// Unbind from the service
+		Log.d(TAG, "#### STOP: mIsBound: " + mIsBound + " mService: " + mService);
+		if (mIsBound) {
+			unbindService(mConnection);
+			mIsBound = false;
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
 	}
 }
